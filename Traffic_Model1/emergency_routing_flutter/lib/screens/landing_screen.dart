435bc249967e542b8a448b56dd55e53867a1f8c3
Vaibhavi Jaiswal <vaibhavi.jaiswal05@gmail.com>
2026-03-18 00:46:34 +0530

    login as reporter removed

--- Traffic_Model1/emergency_routing_flutter/lib/screens/landing_screen.dart
+++ Traffic_Model1/emergency_routing_flutter/lib/screens/landing_screen.dart
@@ -168,11 +168,6 @@ class LandingScreen extends StatelessWidget {
                     Row(
                       children: [
                         _buildLoginButton(
-                          title: 'Login as Reporter',
-                          onTap: () => _launchURL('http://localhost:5173'),
-                        ),
-                        const SizedBox(width: 16),
-                        _buildLoginButton(
                           title: 'Login as Authority/Dispatcher',
                           onTap: () => _launchURL('http://localhost:5174'),
                         ),
